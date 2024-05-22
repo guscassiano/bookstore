bcf0d47946290a4d37c2b6238a4978774a7b56d4
Gustavo Cassiano <gucpinto26@gmail.com>
2024-05-22 18:01:58 -0300

    testing the viewsets

--- product/factories.py
+++ product/factories.py
@@ -5,11 +5,11 @@ from product.models import Product, Category
 class CategoryFactory(factory.django.DjangoModelFactory): # facory é um espelho dos nossos modelos, esta classe cria os modelos falsos em Django
     title = factory.Faker('pystr')
     slug = factory.Faker('pystr')
-    descrption = factory.Faker('pystr')
+    description = factory.Faker('pystr')
     active = factory.Iterator([True, False]) # este campo irá trocar entre true e false devido ao iterador
 
     class Meta:
-        model: Category  # informamos sempre o modelo que iremos utilizar, neste caso será o Category para criar essa fabrica
+        model = Category  # informamos sempre o modelo que iremos utilizar, neste caso será o Category para criar essa fabrica
 
 class ProductFactory(factory.django.DjangoModelFactory):
     price = factory.Faker('pyint')
@@ -18,12 +18,12 @@ class ProductFactory(factory.django.DjangoModelFactory):
 
     @factory.post_generation # Define um método de geração pós-fábrica. Este método será executado após a criação da instância do modelo.
     def category(self, create, extracted, **kwargs): # create é um sinalizador booleano que indica se a instância está sendo criada ou construída. extracted contém os valores passados durante a criação.
-        if not create: #  Se o modelo não estiver sendo criado (por exemplo, se estiver sendo apenas construído), o método para aqui.
+        if not create: # Se o modelo não estiver sendo criado (por exemplo, se estiver sendo apenas construído), o método para aqui.
             return
 
         if extracted: # Se valores foram passados durante a criação, este bloco é acionado.
-            for category in extracted:
-                self.category.add(category)  # Adiciona as categorias extraídas à instância do ProducyFactory.
+            for categories in extracted:
+                self.category.add(categories)  # Adiciona as categorias extraídas à instância do ProducyFactory.
 
     class Meta:
-        model: Product
+        model = Product
